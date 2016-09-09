d3a3f915eb373c3e8175d8885f006c1922888328
unknown <@ylzhang-ECP.gemptc.com>
2016-09-09 12:29:07 +0800

    infosample.json范例文件，doc前端指南改为使用这个范例文件而不是真实的acc子模块info

--- www/doc/controllers/doc_devWeb.js
+++ www/doc/controllers/doc_devWeb.js
@@ -25,7 +25,7 @@
 
         //读取account的信息文件
         $scope.getAccinfo = function() {
-            var api = 'http://m.xmgc360.com/start/web/account/info.json';
+            var api = 'http://m.xmgc360.com/start/web/infosample.json';
             $.get(api, function(res) {
                 //                console.log('GET', api, null, res);
                 _fns.applyScope($scope, function() {
